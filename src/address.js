ff4e27aa861fce565d07e5b2105496504b4d6e9c
mrzhao <zhaomengru2018@gmail.com>
2022-06-29 10:21:10 +0800

    support tx

--- src/address.js
+++ src/address.js
@@ -13,6 +13,9 @@ function buf2hex(buffer) { // buffer is an ArrayBuffer
 export const getAddress = (path, seed) => {
     const { key } = derivePath(path, seed.toString('hex'));
     const keyPair = nacl.sign.keyPair.fromSeed(key);
+    console.log('------public key---------');
+    console.log(Buffer.from(keyPair.publicKey).toString('hex'));
+    console.log('---------------');
     const publicKey = 'ed25519:' + bs58.encode(Buffer.from(keyPair.publicKey))
     const secretKey = 'ed25519:' + bs58.encode(Buffer.from(keyPair.secretKey))
     return {publicKey, secretKey}
@@ -24,8 +27,12 @@ export const getPrivKey = (path) => {
     return nacl.sign.keyPair.fromSeed(key).secretKey
 }
 
-const path = "m/44'/397'/0'/0'/0'"
+
+const path = "m/44'/397'/0'/0'/1'"
 
 const passphase_path =  "m/44'/397'/0'"
 
-console.log(getAddress(path, bip39.mnemonicToSeedSync(process.env.WORDS)));
+// console.log(getAddress(path, bip39.mnemonicToSeedSync(process.env.WORDS)));
+console.log('------naksh-mrzhao.testnet---------')
+console.log(getAddress("m/44'/397'/0'/0'/1'", bip39.mnemonicToSeedSync(process.env.WORDS)))
+console.log('---------------')
